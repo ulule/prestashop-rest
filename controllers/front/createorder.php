bd2914b5c5945b2b3421c900b55e329c48ab9a35
stifler97 <stifler@binshops.com>
2022-04-19 22:33:18 +0430

    add to cart

--- controllers/front/createorder.php
+++ controllers/front/createorder.php
@@ -9,10 +9,39 @@
  *
  */
 
-require_once dirname(__FILE__) . '/../AbstractRESTController.php';
+require_once dirname(__FILE__) . '/../AbstractCartRESTController.php';
 
-class BinshopsrestCreateorderModuleFrontController extends AbstractRESTController
+class BinshopsrestCreateorderModuleFrontController extends AbstractCartRESTController
 {
+    protected $_customer;
+    protected $_billing_address;
+    protected $_line_items;
+    protected $_shipping_address;
+    protected $_shipping_lines;
+    protected $_note;
+
+    public function init(){
+        $_POST = json_decode(Tools::file_get_contents('php://input'), true);
+        
+        //get payload
+        $this->_customer = Tools::getValue('customer');
+        $this->_billing_address = Tools::getValue('billing_address');
+        $this->_line_items = Tools::getValue('line_items');
+        $this->_shipping_address = Tools::getValue('shipping_address');
+        $this->_shipping_lines = Tools::getValue('shipping_lines');
+        $this->_note = Tools::getValue('note');
+         
+        //needed in updateCart
+        $_GET['update'] = 1;
+        $_GET['op'] = 'up';
+        $_GET['action'] = 'update';
+        $_GET['id_product'] = $this->_line_items[0]['product_id']; 
+        $_GET['id_product_attribute'] = $this->_line_items[0]['id_product_attribute']; 
+        $_GET['qty'] = $this->_line_items[0]['quantity'];
+
+        parent::init();
+    }
+
     protected function processGetRequest()
     {
         $this->ajaxRender(json_encode([
@@ -21,19 +50,9 @@ class BinshopsrestCreateorderModuleFrontController extends AbstractRESTControlle
         ]));
         die;
     }
-
+    
     protected function processPostRequest()
     {
-        $_POST = json_decode(Tools::file_get_contents('php://input'), true);
-
-        //get payload
-        $_customer = Tools::getValue('customer');
-        $_billing_address = Tools::getValue('billing_address');
-        $_line_items = Tools::getValue('line_items');
-        $_shipping_address = Tools::getValue('shipping_address');
-        $_shipping_lines = Tools::getValue('shipping_lines');
-        $_note = Tools::getValue('note');
-        
         $psdata = [];
         
         /**
@@ -48,14 +67,39 @@ class BinshopsrestCreateorderModuleFrontController extends AbstractRESTControlle
             $guestAllowedCheckout
         );
         $customer = new Customer();
-        $customer->email = $_customer['email'];
-        $customer->firstname = $_customer['first_name'];
-        $customer->lastname = $_customer['last_name'];
+        $customer->email = $this->_customer['email'];
+        $customer->firstname = $this->_customer['first_name'];
+        $customer->lastname = $this->_customer['last_name'];
         
         // for later use we can fill password here
         $password = '';
 
         $psdata['registered'] = $customerPresister->save($customer, $password);
+
+        /**
+         * step 2
+         * login
+         */
+
+        //actually no need to do this, already logged in
+        $customer = $this->context->customer;
+        
+        /**
+         * step 3
+         * create address
+         */
+
+        $address = 
+
+        /**
+         * step 4
+         * add to cart
+         */
+
+        $this->updateCart();
+
+        $cartProducts = $this->context->cart->getProducts();
+
 
         $this->ajaxRender(json_encode([
             'success' => true,
@@ -81,5 +125,63 @@ class BinshopsrestCreateorderModuleFrontController extends AbstractRESTControlle
             'message' => 'delete not supported on this path'
         ]));
         die;
+    }
+
+    protected function updateCart()
+    {
+        // Update the cart ONLY if $this->cookies are available, in order to avoid ghost carts created by bots
+        if ($this->context->cookie->exists()
+            && !$this->errors)
+        {
+            if (Tools::getIsset('add') || Tools::getIsset('update')) {
+                $this->processChangeProductInCart();
+            } elseif (Tools::getIsset('delete')) {
+                $this->processDeleteProductInCart();
+            } elseif (CartRule::isFeatureActive()) {
+                if (Tools::getIsset('addDiscount')) {
+                    if (!($code = trim(Tools::getValue('discount_name')))) {
+                        $this->errors[] = $this->trans(
+                            'You must enter a voucher code.',
+                            [],
+                            'Shop.Notifications.Error'
+                        );
+                    } elseif (!Validate::isCleanHtml($code)) {
+                        $this->errors[] = $this->trans(
+                            'The voucher code is invalid.',
+                            [],
+                            'Shop.Notifications.Error'
+                        );
+                    } else {
+                        if (($cartRule = new CartRule(CartRule::getIdByCode($code)))
+                            && Validate::isLoadedObject($cartRule)
+                        ) {
+                            if ($error = $cartRule->checkValidity($this->context, false, true)) {
+                                $this->errors[] = $error;
+                            } else {
+                                $this->context->cart->addCartRule($cartRule->id);
+                            }
+                        } else {
+                            $this->errors[] = $this->trans(
+                                'This voucher does not exist.',
+                                [],
+                                'Shop.Notifications.Error'
+                            );
+                        }
+                    }
+                } elseif (($id_cart_rule = (int) Tools::getValue('deleteDiscount'))
+                    && Validate::isUnsignedId($id_cart_rule)
+                ) {
+                    $this->context->cart->removeCartRule($id_cart_rule);
+                    CartRule::autoAddToCart($this->context);
+                }
+            }
+        } elseif (!$this->isTokenValid() && Tools::getValue('action') !== 'show' && !Tools::getValue('ajax')) {
+            $this->ajaxRender(json_encode([
+                'code' => 301,
+                'success' => false,
+                'message' => 'cookie is not set',
+            ]));
+            die;
+        }
     }
 }
